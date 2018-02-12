9e2d361c41c34552d2f523cd61fe024bc574dd3f
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-02-12 22:55:37 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,10 @@
 # Build your say_hello method here
 def say_hello(name)
-  if (name == nil) {
+  if name == nil
     puts "Hello Ruby Programmer"
-  } else {
+  else
     puts "Hello #{name}!"
-  }
+  end
 
 
 
